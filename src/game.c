0ec4461a1e4b3cac6d92f6d52d9f1d2a1fe35478
SnipeParty <pande135@purdue.edu>
2026-04-21 22:36:40 -0400

    display flickering fix

--- src/game.c
+++ src/game.c
@@ -27,12 +27,16 @@ typedef struct
     int x;
     int y;
     int velocity;
+    int old_y;
+    int old_x;
 } Bird;
 
 typedef struct
 {
     int x;
     int gap_y;
+    int old_x;
+    int old_gap_y;
 } Pipe;
 
 Bird bird;
@@ -45,14 +49,21 @@ void game_init()
     bird.x=60;
     bird.y=160;
     bird.velocity=0;
+    bird.old_x = bird.x;
+    bird.old_y = bird.y;
 
     for(int i=0;i<PIPE_COUNT;i++)
     {
         pipes[i].x=SCREEN_WIDTH+i*120;
         pipes[i].gap_y = rand() % (SCREEN_HEIGHT - PIPE_GAP - 20) + 10; //do not change this it breaks
+        pipes[i].old_x = pipes[i].x;
+        pipes[i].old_gap_y = pipes[i].gap_y;
     }
 
     game_over=false;
+    tft_fill_screen(COLOR_BG);
+    draw_pipes();
+    draw_bird();
 }
 
 void bird_jump()
@@ -63,6 +74,8 @@ void bird_jump()
 
 void update_bird()
 {
+    bird.old_x = bird.x;
+    bird.old_y = bird.y;
     bird.velocity+=GRAVITY;
     bird.y+=bird.velocity;
 
@@ -120,7 +133,10 @@ void draw_bird()
 {
     draw_rect(bird.x,bird.y,BIRD_SIZE,BIRD_SIZE,COLOR_BIRD);
 }
-
+void erase_bird()
+{
+    draw_rect(bird.old_x, bird.old_y, BIRD_SIZE, BIRD_SIZE, COLOR_BG);
+}
 void draw_pipes()
 {
     for(int i=0;i<PIPE_COUNT;i++)
@@ -137,11 +153,27 @@ void draw_pipes()
                   COLOR_PIPE);
     }
 }
+void erase_pipes()
+{
+    for(int i = 0; i < PIPE_COUNT; i++)
+    {
+        int old_x = pipes[i].old_x;
+        int old_gap = pipes[i].old_gap_y;
+
+        draw_rect(old_x, 0, PIPE_WIDTH, old_gap, COLOR_BG);
+
+        draw_rect(old_x,
+                  old_gap + PIPE_GAP,
+                  PIPE_WIDTH,
+                  SCREEN_HEIGHT - (old_gap + PIPE_GAP),
+                  COLOR_BG);
+    }
+}
 
 void game_draw()
 {
-    tft_fill_screen(COLOR_BG);
-
+    erase_pipes();
+    erase_bird();
     draw_pipes();
     draw_bird();
 }
